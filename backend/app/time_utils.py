825c436464227ee4029d3fb5d367158dadbdee55
pete <pgtuff@gmail.com>
2025-01-09 01:16:57 +0000

    fixed tests

--- backend/app/time_utils.py
+++ backend/app/time_utils.py
@@ -17,6 +17,14 @@ class TimeConverter(UnitConverter):
         "year": lambda value: value * 31_557_600,
     }
 
+    def seconds_to_months(value):
+        # Round not working in the lambda.
+        return round(value / 2_629_746)
+
+    def seconds_to_years(value):
+        # Round not working in the lambda.
+        return round(value / 31_557_600)
+    
     FROM_BASE_UNIT = {
         "second": lambda value: value,
         "millisecond": lambda value: value * 1000,
@@ -27,6 +35,6 @@ class TimeConverter(UnitConverter):
         "hour": lambda value: value / 3600,
         "day": lambda value: value / 86400,
         "week": lambda value: value / 604800,
-        "month": lambda value: value / 2_629_746,
-        "year": lambda value: value / 31_557_600,
+        "month": seconds_to_months,
+        "year": seconds_to_years,
     }
